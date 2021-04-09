32cf588badedade251abda1645df2a03b85ff905
agrygorkiewicz <aneta.grygorkiewicz@gmail.com>
2021-04-09 22:48:26 +0200

    wheather app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import userEvent from '@testing-library/user-event';
+
 import React, { useState } from 'react';
 
 const api = {
@@ -6,41 +6,26 @@ const api = {
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 
-
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
-  const search = event => {
-    if (event.key === "Enter") {
+  const search = evt => {
+    if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
-          setQuery('');
           setWeather(result);
-        }); 
-       
-    } 
+          setQuery('');
+          console.log(result);
+        });
+    }
   }
 
-
   const dateBuilder = (d) => {
-    let months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
-    ];
-
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
@@ -49,53 +34,36 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
 
-
-
-  // switch (weather.main) {
-  //   case "Snow":
-  //     document.getElementsByClassName(".app").style.backgroundImage =
-  //       "url('https://mdbgo.io/ascensus/mdb-advanced/img/snow.gif')";
-  //     break;
-  //   case "Clouds":
-  //     document.getElementsByClassName(".app").style.backgroundImage =
-  //       "url('https://mdbgo.io/ascensus/mdb-advanced/img/clouds.gif')";
-  //     break;
-  //   default:
-  //     document.getElementsByClassName(".app").style.backgroundImage =
-  //       "url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')";
-  //     break;
-  // }
-
-
-
   return (
-    <div className="app" id="wrapper-bg">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp < 16) ? 'app cold' : 'app') : 'app'}>
       <main>
         <div className="search-box">
-          <input type="text"
-            className="search"
-            placeholder="Search"
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
         {(typeof weather.main != "undefined") ? (
-          <div>
-            <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp">
-                {Math.round(weather.main.temp)}°c
-            </div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
+        <div className="box">
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°C
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
         ) : ('')}
       </main>
     </div>
   );
 }
+
 export default App;
